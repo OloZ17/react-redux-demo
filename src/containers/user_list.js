9b3398f11f5850ff83e95b8939601a909ce5bcba
OloZ17 <glims@wanadoo.fr>
2019-04-17 14:55:34 +0200

    add reducer

---
+++ src/containers/user_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { object } from "prop-types";
+import { connect } from "react-redux";
+
+class UserList extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="col-md-4">
+          {this.props.myUsers.map(user => {
+            return (
+              <li className="list-group-item" key={user.id}>
+                {user.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+UserList.propTypes = {
+  myUsers: object
+};
+
+var mapStateToProps = state => {
+  return {
+    myUsers: state.users
+  };
+};
+
+// function mapStateToProps(state) {
+//   return {
+//     myUsers: state.users
+//   };
+// }
+
+export default connect(mapStateToProps)(UserList);
