85e516939fe67a8348be43d814ae55ffcdb22e55
OloZ17 <glims@wanadoo.fr>
2019-04-18 10:16:45 +0200

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from "react";
-import UserList from "../containers/user_list";
-import UserDetail from "../containers/user_detail";
+import UserList from "./user_list";
+import UserDetail from "./user_detail";
 import "../assets/style.css";
 import { fetchUsers } from "../services";
 
 export default class App extends Component {
   render() {
-    // return <div>{UserList}</div>;
-    //return <div>toto</div>;
     return (
       <Fragment>
         <UserList myUsers={fetchUsers()} />
