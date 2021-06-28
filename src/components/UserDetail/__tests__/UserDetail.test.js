ad55f6fbbcea3cb9e3d0e0a1d666c363547284f8
Thomas Lamarche <glims@wanadoo.fr>
2021-06-28 13:23:54 +0200

    unit test added continued ..

--- src/components/UserDetail/__tests__/UserDetail.test.js
+++ src/components/UserDetail/__tests__/UserDetail.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 
 import UserDetail from '../UserDetail';
+
+afterEach(cleanup);
 
 describe('Testing UserDetail() Component', () => {
   it('should take a snapshot', () => {
---
+++ src/components/UserList/__tests__/UserList.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  render,
+  screen,
+  cleanup,
+  within,
+  fireEvent,
+} from '@testing-library/react';
+
+import UserList from '../UserList';
+
+afterEach(cleanup);
+
+describe('Testing UserList() Component', () => {
+  const users = [
+    { name: 'Sarah Croche', id: 1 },
+    { name: 'Eléonore  Labanquise', id: 2 },
+    { name: 'Emma Karena', id: 3 },
+    { name: 'Jean Bonnot', id: 4 },
+  ];
+  const selectedUser = jest.fn();
+  it('should take a snapshot', () => {
+    const { asFragment } = render(
+      <UserList users={users} selectedUser={selectedUser} />
+    );
+    expect(asFragment(<UserList />)).toMatchSnapshot();
+  });
+
+  it('should render all items', () => {
+    render(<UserList users={users} selectedUser={selectedUser} />);
+    const list = screen.getByRole('list');
+    const { getAllByRole } = within(list);
+    const items = getAllByRole('listitem');
+    expect(items.length).toBe(4);
+  });
+
+  it('should click', () => {
+    render(<UserList users={users} selectedUser={selectedUser} />);
+    const toClick = screen.getByText('Sarah Croche');
+    fireEvent.click(toClick);
+    expect(selectedUser).toBeCalled();
+  });
+});
